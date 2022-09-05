435d49cea8ef9037f1522eb02f6b79993168df03
Denisse Ortiz Ordaz <denisseortizu@gmail.com>
2022-09-05 09:51:33 -0500

    HelloWorld Primera app

---
+++ Unidad-2/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Mac12 on 05/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func PrimeraAccion(_ sender: Any) {
+    }
+    @IBOutlet var Etiqueta1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
