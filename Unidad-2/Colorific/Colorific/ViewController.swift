cb817bc43a343145a66a7828be6a8811ae2b89bd
Denisse Ortiz Ordaz <denisseortizu@gmail.com>
2022-09-08 10:18:56 -0500

    Add files via upload

--- Unidad-2/Colorific/Colorific/ViewController.swift
+++ Unidad-2/Colorific/Colorific/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func changeColor(sender: UIButton){
+        
+        //Generamos nùmeros random
+        let r = CGFloat(arc4random() % 255)
+        let g = CGFloat(arc4random() % 255)
+        let b = CGFloat(arc4random() % 255)
+        
+        //Creamos el nuevo Color
+        let color = UIColor(red:(r/255.0), green: (g/255.0), blue: (b/255.0), alpha: 1.0)
+        
+        //Actualizamos Vista
+        view.backgroundColor = color
+        
+    }
 
 
 }
